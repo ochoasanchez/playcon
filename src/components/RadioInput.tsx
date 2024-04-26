2824f134bab471d0e88381c493479a6b4851a0b0
Juan <juanluis182@gmail.com>
2024-04-26 00:01:26 -0400

    Add animations to trivia

--- src/components/RadioInput.tsx
+++ src/components/RadioInput.tsx
@@ -1,11 +1,12 @@
 type RadioInputProps = {
   id: string,
+  index: number,
   label: string,
   selectedOption: string,
   onChange: (id: string) => void,
 }
 
-const RadioInput = ({ id, label, selectedOption, onChange }: RadioInputProps) => {
+const RadioInput = ({ id, index, label, selectedOption, onChange }: RadioInputProps) => {
   const isSelected = selectedOption === id;
 
   const handleClick = () => {
@@ -14,7 +15,7 @@ const RadioInput = ({ id, label, selectedOption, onChange }: RadioInputProps) =>
 
   return (
     <div 
-      className={`bg-white border-4 ${isSelected ? 'border-orange-500' : 'border-white'} text-lg rounded-md px-3 py-4 mt-4 transition duration-300 hover:border-orange-500 focus-within:border-orange-500 cursor-pointer`} 
+      className={`bg-white border-4 ${isSelected ? 'border-orange-500' : 'border-white'} text-lg rounded-md px-3 py-4 mt-4 transition duration-300 hover:border-orange-500 focus-within:border-orange-500 cursor-pointer animate-slide-in-${index + 1}`} 
       onClick={handleClick}
     >
       <input 
--- src/pages/Trivia.tsx
+++ src/pages/Trivia.tsx
@@ -24,10 +24,10 @@ export function Trivia() {
     }
 
     return (
-        <div className="h-lvh w-full flex flex-col items-center justify-center">
+        <div className="h-lvh w-full flex flex-col items-center justify-center px-4 md:px-0">
             {currentQuestionIndex < questions.length ? (
                 <>
-                    <div id="progressBar" className="flex w-lvw gap-12 px-12">
+                    <div id="progressBar" className="flex w-lvw gap-6 md:gap-12 md:px-12">
                         {questions.map((_, i) => (
                             <div
                                 key={i}
@@ -36,21 +36,23 @@ export function Trivia() {
                         ))}
                     </div>
                     <p className="text-yellow-300 font-bold mt-10 uppercase">Question {currentQuestionIndex + 1} of {questions.length}</p>
-                    <h1 className="text-6xl font-bold text-white mt-4">{questions[currentQuestionIndex].question}</h1>
-
-                    <div className="flex flex-col w-3/12 mt-4">
+                    <h1 key={`title-${currentQuestionIndex}`} className="text-6xl font-bold text-white mt-4 text-left md:text-center animate-slide-in-1">{questions[currentQuestionIndex].question}</h1>
+                            {/* <div className={`w-full bg-yellow-300 h-4 rounded-lg`}></div> */}
+                            
+                    <div className="flex flex-col w-full md:w-8/12 lg:w-3/12 mt-4">
                         <form className="flex flex-col">
                             {questions[currentQuestionIndex].options.map((option, index) => (
                                 <RadioInput
-                                    key={index}
+                                    key={`${currentQuestionIndex}-${index}`}
                                     id={option}
+                                    index={index}
                                     label={option}
                                     selectedOption={selectedOption}
                                     onChange={handleOptionChange}
                                 />
                             ))}
                             <button
-                                className="bg-orange-500 rounded-md mt-4 py-4 font-bold uppercase"
+                                className="bg-orange-500 rounded-md mt-4 py-4 font-bold uppercase animate-slide-in-5"
                                 onClick={handleNextQuestion}
                                 disabled={!selectedOption}
                             >
