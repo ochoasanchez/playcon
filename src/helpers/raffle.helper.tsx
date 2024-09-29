851e0c5844186f98602cf7dc1cbde0045535d9c5
Juan <juanluis182@gmail.com>
2024-09-29 14:13:26 -0400

    Making code prettier

--- src/helpers/raffle.helper.tsx
+++ src/helpers/raffle.helper.tsx
@@ -13,15 +13,6 @@ const signUpParticipant = async (data: ParticipantType) => {
   // const scoreData = { data: data };
   const participantData = { data };
 
-<<<<<<< HEAD
-    const participantData = { data };
-
-    try {
-      const response = await axios.post(`${strapiUrl}/api/participants`, participantData, config);
-      console.log('signUpResponse:', response)
-      
-      return response.data;
-=======
   try {
     const response = await axios.post(
       `${strapiUrl}/api/participants`,
@@ -34,26 +25,11 @@ const signUpParticipant = async (data: ParticipantType) => {
     return response.data;
   } catch (error) {
     console.error("Error sending score data:", error);
->>>>>>> prettier
 
     return error;
   }
 };
 
-<<<<<<< HEAD
-      return error;
-    }
-  };
-
-  
-
-const getRaffleParticipants = async (raffleType?: 'main' | 'trivia' | 'memory') => {
-  if (!raffleType || raffleType === 'main') {
-    try {
-      const response = await axios.get(`${strapiUrl}/api/clients`, config);
-      console.log('Participants:', response.data)
-
-=======
 const getRaffleParticipants = async (
   raffleType?: "main" | "trivia" | "memory",
 ) => {
@@ -64,7 +40,6 @@ const getRaffleParticipants = async (
       // const response = await axios.get(`${strapiUrl}/api/clients?filters[level][$eq]=3`, config);
       console.log("Participants:", response.data);
       // debugger;
->>>>>>> prettier
       return response.data;
     } catch (error) {
       console.error("Error getting participants data:", error);
@@ -72,13 +47,6 @@ const getRaffleParticipants = async (
       return error;
     }
   }
-<<<<<<< HEAD
-  if (raffleType === 'memory') {
-    try {
-      const response = await axios.get(`${strapiUrl}/api/scores?filters[game][$eq]=memory`, config);
-      console.log('Memory Participants:', response.data);
-
-=======
   if (raffleType === "memory") {
     // debugger;
     try {
@@ -89,27 +57,10 @@ const getRaffleParticipants = async (
       );
       console.log("Memory Participants:", response.data);
       // debugger;
->>>>>>> prettier
       return response.data;
     } catch (error) {
-<<<<<<< HEAD
-      console.error('Error getting memory participants data:', error);
-  
-      return error;
-    }
-  } if (raffleType === 'trivia') {
-    try {
-      const response = await axios.get(`${strapiUrl}/api/scores?filters[game][$eq]=trivia&filters[scoreValue][$gte]=4`, config);
-      console.log('Trivia Participants:', response.data)
-      return response.data;
-  
-    } catch (error) {
-      console.error('Error getting trivia participants data:', error);
-  
-=======
       console.error("Error getting memory participants data:", error);
 
->>>>>>> prettier
       return error;
     }
   }
