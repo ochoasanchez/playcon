95ca2a4f9cfd91353aa808f5c5519369432cdf73
Juan <juanluis182@gmail.com>
2024-10-28 12:17:05 -0400

    Add edit capabalities to Strapi sync page

--- src/pages/Sync.tsx
+++ src/pages/Sync.tsx
@@ -1,35 +1,83 @@
+import { useState } from "react";
+import axios from "axios";
 import { ActionButton } from "../components/ActionButton";
 import FooterIcons from "../components/FooterIcons";
 import Logo from "../components/Logo";
 import { MainSubtitle } from "../components/MainSubitle";
 import { MainTitle } from "../components/MainTitle";
-import { updateUsers } from "../helpers/game.helper";
+import { StrapiSettingsModal } from "../components/StrapiSettingsModal";
 
+let initialStrapiUrl = import.meta.env.VITE_STRAPI_URL;
+let initialBearerToken = `Bearer ${import.meta.env.VITE_STRAPI_TOKEN}`;
 
 export function Sync() {
+  const [strapiUrl, setStrapiUrl] = useState(initialStrapiUrl);
+  const [bearerToken, setBearerToken] = useState(initialBearerToken);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // const syncData = () => {
-    //     console.log(JSON.parse(localStorage.getItem('users') || '[]'))
-    // }
+  const updateUsers = async () => {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    const config = {
+      headers: {
+        Authorization: bearerToken,
+      },
+    };
 
-    
+    try {
+      let counter = 0;
+      for (const user of users) {
+        const data = { data: user };
+        const response = await axios.post(`${strapiUrl}/api/clients`, data, config);
+        console.log("User successfully synced: ", response);
+        counter++;
+      }
+      console.log(`Looped through ${counter} users`);
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
+
+  const updateStrapiHeaders = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleSaveSettings = (url: string, token: string) => {
+    setStrapiUrl(url);
+    setBearerToken(token);
+  };
+
   return (
-    <main className="animate-fade-in gap-24 px-12">
-  
+    <main className="animate-fade-in gap-4 lg:gap-16 px-2 lg:px-12">
       <Logo />
-
-      <MainTitle text="Respaldo de datos" uppercase/>
+      <MainTitle text="Respaldo de datos" uppercase />
       <MainSubtitle text="Presiona el botón para enviar los datos a Strapi" />
 
-      <div className="flex flex-col gap-12">
-      <ActionButton url="/sync" text="Sincronizar datos" onClick={updateUsers}/>
+      <div className="flex flex-col w-full truncate bg-white text-orange-500 px-1">
+        <p className="text-2xl lg:text-5xl">
+          <span className="uppercase underline">Strapi URL:</span> {strapiUrl}
+        </p>
+        <br />
+        <p className="text-2xl lg:text-5xl text-ellipsis">
+          <span className="uppercase underline">Token:</span> {bearerToken}
+        </p>
+      </div>
 
-      <ActionButton url="/dashboard" text="Volver" className="btn-alternate"/>
-      {/* <ActionButton url="/sorteo/trivia" text="Lista de usuarios" />
-      <ActionButton url="/sorteo/trivia" text="Datos de uso" /> */}
+      <div className="flex flex-col gap-4 lg:gap-12">
+        <ActionButton text="Sincronizar datos" onClick={updateUsers} />
+        <ActionButton text="Editar datos Strapi" onClick={updateStrapiHeaders} />
+        <ActionButton url="/dashboard" text="Volver" className="btn-alternate" />
       </div>
 
       <FooterIcons />
+
+      {isModalOpen && (
+        <StrapiSettingsModal
+          currentUrl={strapiUrl}
+          currentToken={bearerToken}
+          onSave={handleSaveSettings}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
     </main>
   );
 }
