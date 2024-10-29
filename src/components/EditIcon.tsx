5b4297a28fd4bb82fbbb90a5f2380d2fa53ecff9
Juan <juanluis182@gmail.com>
2024-10-28 20:27:54 -0400

    Update Sync page with backup alternatives

---
+++ src/components/EditIcon.tsx
@@ -0,0 +1,5 @@
+import edit from "../assets/icons/edit.svg";
+
+export default function EditIcon() {
+    return <img src={edit} className="w-12 mx-auto"/>
+}
--- src/pages/Sync.tsx
+++ src/pages/Sync.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
 import { ActionButton } from "../components/ActionButton";
-import FooterIcons from "../components/FooterIcons";
 import Logo from "../components/Logo";
-import { MainSubtitle } from "../components/MainSubitle";
 import { MainTitle } from "../components/MainTitle";
 import { StrapiSettingsModal } from "../components/StrapiSettingsModal";
+import EditIcon from "../components/EditIcon";
 
 let initialStrapiUrl = import.meta.env.VITE_STRAPI_URL;
 let initialBearerToken = `Bearer ${import.meta.env.VITE_STRAPI_TOKEN}`;
@@ -15,8 +14,9 @@ export function Sync() {
   const [bearerToken, setBearerToken] = useState(initialBearerToken);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const users = JSON.parse(localStorage.getItem('users') || '[]');
+
   const updateUsers = async () => {
-    const users = JSON.parse(localStorage.getItem("users") || "[]");
     const config = {
       headers: {
         Authorization: bearerToken,
@@ -46,8 +46,7 @@ export function Sync() {
     setBearerToken(token);
   };
 
-  const handleShareData = () => {
-    const users = JSON.parse(localStorage.getItem('users') || '[]');
+  const handleShareDataToMail = () => {
     const jsonString = JSON.stringify(users, null, 2); // Format JSON for readability
   
     // Use encodeURIComponent to handle special characters in JSON
@@ -56,31 +55,70 @@ export function Sync() {
     // Open native mail app with prefilled email
     window.location.href = mailToLink;
   };
+  
+  const handleShare = async () => {
+    
+    const shareData = {
+      title: 'Exported Data',
+      text: JSON.stringify(users, null, 2) // Pretty-print the JSON
+    };
+
+    try {
+      await navigator.share(shareData);
+      console.log('Data shared successfully!');
+    } catch (error) {
+      console.error('Sharing failed', error);
+    }
+  };
+
+  const handleDownload = () => {
+    const dataStr = JSON.stringify(users, null, 2);
+    const blob = new Blob([dataStr], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+  
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'exported-data.json';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  
+    URL.revokeObjectURL(url); // Clean up the URL after download
+  };
+  
+  
 
   return (
     <main className="animate-fade-in gap-4 sm:gap-16 px-2 sm:px-12">
       <Logo />
       <MainTitle text="Respaldo de datos" uppercase />
-      <MainSubtitle text="Presiona el botón para enviar los datos a Strapi" />
 
-      <div className="flex flex-col w-full truncate bg-white text-orange-500 px-1">
-        <p className="text-2xl sm:text-5xl">
-          <span className="uppercase underline">Strapi URL:</span> {strapiUrl}
-        </p>
-        <br />
-        <p className="text-2xl sm:text-5xl text-ellipsis">
-          <span className="uppercase underline">Token:</span> {bearerToken}
-        </p>
+      <div className="flex w-full gap-4">
+        <div className="flex flex-col w-full truncate bg-white text-orange-500 px-1">
+          <p className="text-2xl sm:text-5xl">
+            <span className="uppercase underline">Strapi URL:</span> {strapiUrl}
+          </p>
+          <br />
+          <p className="text-2xl sm:text-5xl text-ellipsis">
+            <span className="uppercase underline">Token:</span> {bearerToken}
+          </p>
+        </div>
+        
+        <button onClick={updateStrapiHeaders}>
+          <EditIcon />
+        </button>
+        
       </div>
 
       <div className="flex flex-col gap-4 sm:gap-12">
-        <ActionButton text="Sincronizar datos" onClick={updateUsers} />
-        <ActionButton text="Editar datos Strapi" onClick={updateStrapiHeaders} />
-        <ActionButton text="Enviar datos por correo" onClick={handleShareData} />
+        <ActionButton text="Enviar datos a Strapi" onClick={updateUsers} />
+        <ActionButton text="Enviar datos por correo" onClick={handleShareDataToMail} />
+        <ActionButton text="Compartir datos a otra app" onClick={handleShare} />
+        <ActionButton text="Descargar datos" onClick={handleDownload} />
         <ActionButton url="/dashboard" text="Volver" className="btn-alternate" />
       </div>
 
-      <FooterIcons />
+      {/* <FooterIcons /> */}
 
       {isModalOpen && (
         <StrapiSettingsModal
