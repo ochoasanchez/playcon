f1f66d88ae5ca555dff9cbc4a7b420de3e581538
Juan <juanluis182@gmail.com>
2025-05-11 11:10:11 -0400

    Deploy 6

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['vite.svg', 'logo-192.png', 'logo-512.png'],
       manifest: {
-        name: 'FC App',
-        short_name: 'FC',
+        name: 'Megalabs',
+        short_name: 'Megalabs',
         description: 'App de interactiva de trivia y memoria',
         theme_color: '#33d9b2',
         icons: [
