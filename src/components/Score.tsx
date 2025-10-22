de4339fbfe176b01cb91f75de47da41ef4320452
Juan <juanluis182@gmail.com>
2025-10-21 21:53:51 -0400

    PHK v1.4.1

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -24,9 +24,9 @@ const Score = ({
     >
       <div className="flex h-3/5 gap-y-6 flex-col items-center justify-center">
         <img src={image} className="w-6/12" alt="Result" />
-        <p className="w-10/12 text-5xl sm:text-5xl text-emerald-700 font-semibold">{result}</p>
+        <p className="w-10/12 text-5xl sm:text-5xl text-blue-700 font-semibold">{result}</p>
       </div>
-      <div className="score-circle flex h-2/5 gap-y-8 bg-emerald-500 justify-between">
+      <div className="score-circle flex h-2/5 gap-y-8 bg-blue-500 justify-between">
         <div className="w-8/12 pt-16">
           
       <h1 className={`text-center font-bold playfair-display-400 text-4xl`}>
